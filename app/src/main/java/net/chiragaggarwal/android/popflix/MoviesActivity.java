d01b20b2ab41b7f2a1aad1f01672f759547133b2
Chirag Aggarwal <me@chi6rag.net>
2016-01-31 18:48:56 +0530

    Adding custom toolbar with refresh button for Movies Screen

--- app/src/main/java/net/chiragaggarwal/android/popflix/MoviesActivity.java
+++ app/src/main/java/net/chiragaggarwal/android/popflix/MoviesActivity.java
@@ -1,13 +1,32 @@
 package net.chiragaggarwal.android.popflix;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MoviesActivity extends AppCompatActivity {
+
+    private static final String POP_FLIX = "PopFlix";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+        initializeToolbar();
+    }
+
+    private void initializeToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.default_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(POP_FLIX);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.movies, menu);
+        return true;
     }
 }
