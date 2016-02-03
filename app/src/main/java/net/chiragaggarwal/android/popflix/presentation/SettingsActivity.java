b1974c36b0b536c2ddfd9c81f98ff7b3aede6a7f
Chirag Aggarwal <me@chi6rag.net>
2016-02-03 15:07:28 +0530

    Set preference value as summary

--- app/src/main/java/net/chiragaggarwal/android/popflix/presentation/SettingsActivity.java
+++ app/src/main/java/net/chiragaggarwal/android/popflix/presentation/SettingsActivity.java
@@ -1,14 +1,50 @@
 package net.chiragaggarwal.android.popflix.presentation;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 import net.chiragaggarwal.android.popflix.R;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements
+        Preference.OnPreferenceChangeListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+
+        setPreferenceSummaryAsValue(sortOrderPreference());
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String selectedEntry = getSelectedEntryCorrespondingToEntryValue(preference, (String) newValue);
+        preference.setSummary(selectedEntry);
+        return true;
+    }
+
+    private void setPreferenceSummaryAsValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+        String preferenceValue = PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), "");
+        onPreferenceChange(preference, preferenceValue);
+    }
+
+    private String getSelectedEntryCorrespondingToEntryValue(Preference preference, String newValue) {
+        ListPreference listPreference = ((ListPreference) preference);
+        CharSequence[] listPreferenceEntries = listPreference.getEntries();
+        CharSequence[] listPreferenceEntryValues = listPreference.getEntryValues();
+
+        for (Integer preferenceIndex = 0; preferenceIndex < listPreferenceEntries.length; preferenceIndex++) {
+            if (newValue.equals(listPreferenceEntryValues[preferenceIndex]))
+                return (String) listPreferenceEntries[preferenceIndex];
+        }
+        return "";
+    }
+
+    private Preference sortOrderPreference() {
+        String sortOrderPreferenceKey = getString(R.string.preference_sort_order_key);
+        return findPreference(sortOrderPreferenceKey);
     }
 }
