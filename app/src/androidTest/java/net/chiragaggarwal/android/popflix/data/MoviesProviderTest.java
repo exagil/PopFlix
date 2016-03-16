7a2bc2bb0fae27da4108f9066548ccebbccd8699
Chirag Aggarwal <me@chi6rag.net>
2016-03-16 06:06:26 +0530

    Making Movies Provider Exported

--- app/src/androidTest/java/net/chiragaggarwal/android/popflix/data/MoviesProviderTest.java
+++ app/src/androidTest/java/net/chiragaggarwal/android/popflix/data/MoviesProviderTest.java
@@ -1,6 +1,7 @@
 package net.chiragaggarwal.android.popflix.data;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.test.AndroidTestCase;
@@ -24,5 +25,14 @@ public class MoviesProviderTest extends AndroidTestCase {
         ComponentName moviesProviderComponent = new ComponentName(getContext().getPackageName(), MoviesProvider.class.getName());
         ProviderInfo moviesProviderInfo = getContext().getPackageManager().getProviderInfo(moviesProviderComponent, 0);
         assertEquals(moviesProviderInfo.authority, PopFlixContract.MoviesEntry.PROVIDER_AUTHORITY);
+    }
+
+    @Test
+    public void shouldBeExported() throws PackageManager.NameNotFoundException {
+        Context context = getContext();
+        String packageName = context.getPackageName();
+        ComponentName moviesProviderComponent = new ComponentName(packageName, MoviesProvider.class.getName());
+        ProviderInfo moviesProviderInfo = context.getPackageManager().getProviderInfo(moviesProviderComponent, 0);
+        assertTrue(moviesProviderInfo.exported);
     }
 }
