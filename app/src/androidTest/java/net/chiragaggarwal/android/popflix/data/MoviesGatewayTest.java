c9f5b5c0e18c28702b2a3ef827e093577b18f31a
Chirag Aggarwal <me@chi6rag.net>
2016-03-25 10:22:20 +0530

    Refactoring Tests

--- app/src/androidTest/java/net/chiragaggarwal/android/popflix/data/MoviesGatewayTest.java
+++ app/src/androidTest/java/net/chiragaggarwal/android/popflix/data/MoviesGatewayTest.java
@@ -14,12 +14,14 @@ import java.util.Date;
 public class MoviesGatewayTest extends AndroidTestCase {
     private Context context;
     private SQLiteDatabase database;
+    private MoviesGateway moviesGateway;
 
     @Override
     protected void setUp() throws Exception {
         this.context = getContext();
         this.database = DatabaseHelper.getInstance(context).getWritableDatabase();
         this.database.beginTransaction();
+        this.moviesGateway = MoviesGateway.getInstance(this.database);
     }
 
     @Override
@@ -29,16 +31,14 @@ public class MoviesGatewayTest extends AndroidTestCase {
 
     @Test
     public void shouldHavePositiveCountWhenMoviesArePresent() {
-        MoviesGateway moviesGateway = MoviesGateway.getInstance(this.database);
         Movie movie = new Movie(1, "Example", new Date(), "nothing/everything", 12.34, 12.34, "example", true);
         ContentValues movieContentValues = movie.toContentValues();
-        long insert = this.database.insert(PopFlixContract.MoviesEntry.TABLE_NAME, null, movieContentValues);
+        this.database.insert(PopFlixContract.MoviesEntry.TABLE_NAME, null, movieContentValues);
         assertEquals(1, moviesGateway.getCount());
     }
 
     @Test
     public void shouldHaveZeroCountWhenNoMoviesPresent() {
-        MoviesGateway moviesGateway = MoviesGateway.getInstance(this.database);
         assertEquals(0, moviesGateway.getCount());
     }
 }
